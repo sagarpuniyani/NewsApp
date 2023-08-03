98becd1b82ffb7f4a53ca339f252b7b978b27d40
Sagar <04017711721_iot@vips.edu>
2023-08-03 21:48:56 +0530

    News is created in the App

--- controller/news-controller.js
+++ controller/news-controller.js
@@ -5,10 +5,10 @@ function showNewsInCard(eachnews){
     // Rendering of the header component of the DOM 
     
     const title_header  = document.createElement('div');
-    title_header.className = 'row title-header';
+    title_header.className = 'row m-2title-header';
     
     const title = document.createElement('h3');
-    title.className = 'title col-10';
+    title.className = 'title m-2 col-10';
     title.innerHTML = eachnews.title;
 
     //making Childs of the title-header
@@ -17,23 +17,23 @@ function showNewsInCard(eachnews){
     document.querySelector('#output').appendChild(title_header);
 
     const mainTag = document.createElement('main');
-    mainTag.className = 'row ';
+    mainTag.className = 'row m-2 ';
 
     const article = document.createElement('div');
-    article.className = 'col-7 row article';
+    article.className = 'col-7 m-2  row article';
 
     const content = document.createElement('div');
-    content.className = 'col-12  content';
+    content.className = 'col-12 m-2 content';
     content.innerHTML = eachnews.content;
 
     const description = document.createElement('div');
-    description.className = 'col-12  description';
+    description.className = 'col-12 m-2  description';
     description.innerHTML = eachnews.description;
 
     const img = document.createElement('img');
     img.alt = 'img';
-    img.class = 'col-5'
-    img.src = eachnews.url;
+    img.class = 'col-5 m-1 '
+    img.src = eachnews.urlToimage;
 
     mainTag.appendChild(article);
     mainTag.appendChild(img);
@@ -44,10 +44,29 @@ function showNewsInCard(eachnews){
     document.querySelector('#output').appendChild(mainTag);
 
 
+    // makin of the about section of the news 
 
+    const about = document.createElement('div');
+    about.className = 'row m-1  about';
 
+    const aWrap = document.createElement('a');
+    aWrap.className = 'col-7'
+    aWrap.href = eachnews.url;
 
+    const btn = document.createElement('button');
+    btn.className = 'col-7 m-2  btn btn-info';
+    btn.innerHTML = 'Read More>>>'
 
+    const author = document.createElement('div');
+    author.className = 'border col-5 author';
+    author.innerHTML = ' By - '+ eachnews.author;
+
+    about.appendChild(aWrap);
+    about.appendChild(author);
+
+    aWrap.appendChild(btn);
+
+    document.querySelector('#output').appendChild(about);
 
 
     
